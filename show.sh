1546eb951e7b37d0bef98fea8b9c59318c146087
Yopiter <Martinius.schneider@web.de>
2018-02-15 17:10:27 +0100

    corrected HTML-Response

--- show.sh
+++ show.sh
@@ -3,4 +3,4 @@ echo "Content-type: text/html; charset=UTF-8"
 echo  
 echo "<html><body>"
 echo "<pre>$(cat /home/awkologist/BioLinux/blastalert-www.txt)</pre>"
-echo "</html></body>"
+echo "</body></html>"
